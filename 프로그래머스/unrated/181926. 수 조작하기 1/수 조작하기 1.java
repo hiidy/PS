92a01dff63cb0fa730137ec12cf3613bdd4e67ee
Woong <83386112+hiidy@users.noreply.github.com>
2023-04-30 19:37:30 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.01 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        for (char c: control.toCharArray()) {
+            if (c == 'w') {
+                n += 1;
+            } else if (c == 's') {
+                n -= 1;
+            } else if (c == 'd') {
+                n += 10;
+            } else if (c == 'a') {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
