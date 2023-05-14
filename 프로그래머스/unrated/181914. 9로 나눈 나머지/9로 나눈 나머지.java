d5b5d30b9132a4095f47698e158b815e09ed34e4
Woong <83386112+hiidy@users.noreply.github.com>
2023-05-14 16:03:10 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        
+        int sum = 0;
+        
+        for (char c : number.toCharArray()) {
+            sum = sum + Character.getNumericValue(c);
+        }
+        return sum % 9;
+    }
+}
