cff9dd885820bfba64e45a45a0760bf380d586e3
Woong <83386112+hiidy@users.noreply.github.com>
2023-06-21 15:51:19 +0900

    [unrated] Title: 배열 조각하기, Time: 0.07 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        
+        for (int i = 0; i < query.length; i++) {
+            int index = query[i];
+            if (i % 2 == 0) {
+                arr = Arrays.copyOfRange(arr, 0, index+1);
+            } else {
+                arr = Arrays.copyOfRange(arr, index, arr.length);
+            }
+        }
+        return arr;
+    }
+}
