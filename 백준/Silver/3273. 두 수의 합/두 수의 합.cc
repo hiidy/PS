44d49fb604913417d47f8af6b0d59076156eb4dc
Woong <83386112+hiidy@users.noreply.github.com>
2023-07-06 18:01:24 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 7760 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,22 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
+int number[1000001] = {};
 
-int a[1000001]={};
-bool occur[2000001];
-int n, x;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x;
 
-  int ans = 0;
-  cin >> n;
-  for(int i = 0; i < n; i++) cin >> a[i];
-  cin >> x;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> number[i];
+    }
+    cin >> x;
+    bool count[2000000];
 
-  for (int i = 0; i < n; i++) {
-    if(x-a[i] > 0 && occur[x-a[i]]) ans++;
-    occur[a[i]] = true;
-  }
-  cout << ans;
+    int ans=0;
+
+    for(int i = 0; i < n; i++) {
+        if(x - number[i] > 0 && count[x - number[i]]) {
+            ans++;
+        } else {
+        count[number[i]] = 1;
+        }
+    }
+
+    cout << ans;
 }
