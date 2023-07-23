74b2ddcf000ec7ed425cf7e8c0832b624ccb3b9b
Woong <83386112+hiidy@users.noreply.github.com>
2023-07-23 13:34:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+
+int dist[100001];
+int n, k;
+queue<int> q;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> k;
+    fill(dist, dist + 100001 , -1);
+    q.push(n);
+    dist[n] = 0;
+    while(dist[k] == -1) {
+        auto cur = q.front();
+        q.pop();
+        for(int nx : {cur+1, cur-1, 2*cur}) {
+            // if(nx == k) {
+            //     cout << dist[cur] + 1;
+            //     return 0;
+            // }
+            if(nx >= 100001 || nx < 0) {
+                continue;
+            }
+            if(dist[nx] != -1) {
+                continue;
+            }
+            dist[nx] = dist[cur] + 1;
+            q.push(nx);
+        }
+    }
+    cout << dist[k];
+}
