4daf6aa02f718523056c416d3652693a1b10b337
Woong <83386112+hiidy@users.noreply.github.com>
2023-07-29 01:24:06 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -1,7 +1,9 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     while(1) {
         int a,b;
         cin >> a >> b;
