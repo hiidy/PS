f58626d93bccb68217abf4ca7c928c5a14635439
Woong <83386112+hiidy@users.noreply.github.com>
2023-07-28 13:01:54 +0900

    [Silver IV] Title: 카드 놓기, Time: 0 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.cc
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string> v;
+bool visited[11];
+set<string> ans;
+vector<string> perm;
+int n, k;
+
+void dfs(int depth) {
+    if(depth == k) {
+        string perm_string;
+        for(auto s : perm) {
+            perm_string+=s;
+        }
+        ans.insert(perm_string);
+        return;
+    }
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == true) {
+            continue;
+        }
+        visited[i] = true;
+        perm.push_back(v[i]);
+        dfs(depth + 1);
+        perm.pop_back();
+        visited[i] = false;
+
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    cin >> k;
+    for(int i = 0; i < n; i++) {
+        string input;
+        cin >> input;
+        v.push_back(input);
+    }
+    dfs(0);
+    cout << ans.size();
+
+
+}
