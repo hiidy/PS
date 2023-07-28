48a9f4620fa70ce6374afd68481ac07a01486e59
Woong <83386112+hiidy@users.noreply.github.com>
2023-07-28 17:38:15 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000002;
+int dp[MAX];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    dp[1] = 0;
+    for(int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + 1;
+        if(i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i/3]+1);
+        }
+        if(i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i/2]+1);
+        }
+    }
+    cout << dp[n];
+}
