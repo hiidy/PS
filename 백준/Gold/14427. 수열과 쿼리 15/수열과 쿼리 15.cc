c7b5b8e7547e5c71f90b43eb5867a638136abf7b
Woong <83386112+hiidy@users.noreply.github.com>
2023-08-13 19:10:43 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 48 ms, Memory: 3456 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.cc
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.cc
@@ -5,33 +5,46 @@
 
 using namespace std;
 const int INF = 1e9 + 1;
+vector<int> tree;
+vector<int> a;
 
 int n, m;
 
+int minIdx(int x, int y) {
+    if (a[x] == a[y]) {
+        return min(x, y);
+    }
+    if (a[x] > a[y]) {
+        return y;
+    } else {
+        return x;
+    }
+}
+
 void init(vector<int> &a, vector<int> &tree, int node, int start, int end) {
     if (start == end) {
-        tree[node] = a[start];
+        tree[node] = start;
     } else {
         int mid = (start + end) / 2;
         init(a, tree, 2 * node, start, mid);
         init(a, tree, 2 * node + 1, mid + 1, end);
-        tree[node] = min(tree[2 * node], tree[2 * node + 1]);
+        tree[node] = minIdx(tree[2 * node], tree[2 * node + 1]);
     }
 }
 
-int query(vector<int> &tree, int node, int start, int end, int left,
-          int right) {
-    if (left > end || right < start) {
-        return INF;
-    }
-    if (left <= start && end <= right) {
-        return tree[node];
-    }
-    int mid = (start + end) / 2;
-    int lsum = query(tree, 2 * node, start, mid, left, right);
-    int rsum = query(tree, 2 * node + 1, mid + 1, end, left, right);
-    return min(lsum, rsum);
-}
+// int query(vector<int> &tree, int node, int start, int end, int left,
+//           int right) {
+//     if (left > end || right < start) {
+//         return INF;
+//     }
+//     if (left <= start && end <= right) {
+//         return tree[node];
+//     }
+//     int mid = (start + end) / 2;
+//     int lsum = query(tree, 2 * node, start, mid, left, right);
+//     int rsum = query(tree, 2 * node + 1, mid + 1, end, left, right);
+//     return minIdx(lsum, rsum);
+// }
 
 void update(vector<int> &a, vector<int> &tree, int node, int start, int end,
             int index, int val) {
@@ -40,27 +53,27 @@ void update(vector<int> &a, vector<int> &tree, int node, int start, int end,
     }
     if (start == end) {
         a[index] = val;
-        tree[node] = val;
+        tree[node] = start;
         return;
     }
     int mid = (start + end) / 2;
     update(a, tree, 2 * node, start, mid, index, val);
     update(a, tree, 2 * node + 1, mid + 1, end, index, val);
-    tree[node] = min(tree[2 * node], tree[2 * node + 1]);
+    tree[node] = minIdx(tree[2 * node], tree[2 * node + 1]);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
-    vector<int> a(n);
+    a.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     cin >> m;
     int H = int(ceil(log2(n)));
     int tree_size = 1 << (H + 1);
-    vector<int> tree(tree_size);
+    tree.resize(tree_size);
     init(a, tree, 1, 0, n - 1);
     while (m--) {
         int input;
@@ -70,8 +83,7 @@ int main() {
             cin >> b >> c;
             update(a, tree, 1, 0, n - 1, b - 1, c);
         } else if (input == 2) {
-            int result = query(tree, 1, 0, n - 1, 0, n - 1);
-            cout << find(a.begin(), a.end(), result) - a.begin() + 1 << "\n";
+            cout << tree[1] + 1 << "\n";
         }
     }
 }
