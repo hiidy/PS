d1a42e2f944b79898e8ddb3793983d4e56d09a47
Woong <83386112+hiidy@users.noreply.github.com>
2023-08-15 19:50:03 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 12 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+const int MOD = 1e9 + 9;
+const int MAX = 1e6 + 1;
+ll dp[MAX];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i < MAX; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD;
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << dp[n] % MOD << "\n";
+    }
+}
