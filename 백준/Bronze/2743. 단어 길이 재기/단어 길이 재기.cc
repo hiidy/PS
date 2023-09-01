286a01eaf01cb1bc0731c2225230100a28d5af29
Woong <83386112+hiidy@users.noreply.github.com>
2023-09-02 03:16:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string input;
+    cin >> input;
+    cout << input.length();
+
+}
